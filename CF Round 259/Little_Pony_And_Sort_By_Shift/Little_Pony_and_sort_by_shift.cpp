99f57822d00879f00c86b4349cb89a96ff3b99e7
Sayak Ghosh <sayakmemari@gmail.com>
2021-11-05 18:22:25 +0530

    Round 259

---
+++ CF Round 259/Little_Pony_And_Sort_By_Shift/Little_Pony_and_sort_by_shift.cpp
@@ -0,0 +1,79 @@
+/*    AUTHOR :- Sayak Ghosh(Username - sayak04)    * /
+/*    College- JGEC   */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define MOD 10000007
+#define pb emplace_back
+                     
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll dig(ll num) {
+    ll sum = 0, rem = 0;
+    while (num > 0) {
+        rem = num % 10;
+        sum += rem;
+        num = num / 10;
+    }
+    return sum;
+}
+int most_significant_bit(ll n) {
+    int pos = 0, temp = 0;
+    while(n>0) {
+        if(n&1>0)
+            pos = temp;
+        temp++;
+        n = n >> 1;
+    }
+    return pos;
+}
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    loop(i, n) cin >> arr[i];
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        if(arr[i]<arr[i-1])
+            cnt = i;
+    }
+    vector<int> newArr = arr;
+    sort(newArr.begin(), newArr.end());
+    rotate(arr.begin(), arr.begin() + cnt, arr.end());
+    if(cnt==0)
+        cout << 0 << endl;
+    else if(arr==newArr)
+        cout << n - cnt << endl;
+    else
+        cout << -1 << endl;
+}
+                
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+   
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+auto start = std::chrono::system_clock::now() ;
+
+int t = 1;
+// cin >> t;
+while (t--)
+{
+    solve();
+    }
+// -----for run-time calculation -------//
+auto end = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds = end-start;
+std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+#ifndef ONLINE_JUDGE
+cout<<"RUN TIME : "<<elapsed_seconds.count()<<"s"<<endl;
+#endif
+//---------//
+    return 0;
+}
