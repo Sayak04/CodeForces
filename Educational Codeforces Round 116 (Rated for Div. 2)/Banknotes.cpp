c33fdab0e65484719129666cd10d5ef62beac0f5
Sayak Ghosh <77233215+Sayak04@users.noreply.github.com>
2021-11-10 17:21:52 +0530

    Create Banknotes.cpp

---
+++ Educational Codeforces Round 116 (Rated for Div. 2)/Banknotes.cpp
@@ -0,0 +1,81 @@
+/*    AUTHOR :- Sayak Ghosh(Username - sayak04)    * /
+/*    College- JGEC   */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define MOD 10000007
+#define pb emplace_back
+                     
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll dig(ll num) {
+    ll sum = 0, rem = 0;
+    while (num > 0) {
+        rem = num % 10;
+        sum += rem;
+        num = num / 10;
+    }
+    return sum;
+}
+int most_significant_bit(ll n) {
+    int pos = 0, temp = 0;
+    while(n>0) {
+        if(n&1>0)
+            pos = temp;
+        temp++;
+        n = n >> 1;
+    }
+    return pos;
+}
+void solve() {
+    ll n;
+    ll k;
+    cin >> n >> k;
+    k += 1;
+    ll arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        arr[i] = pow(10, arr[i]);
+    }
+    ll ans = 0;
+    vector<ll> vec(n);
+    for (int i = 0; i < n - 1; i++)
+        vec[i] = (arr[i + 1] / arr[i]) - 1;
+    for (int i = 0; i < n - 1; i++) {
+        ll temp = min(vec[i], k);
+        k -= temp;
+        ans += temp * arr[i];
+    }
+    if(k>0)
+        ans += k * arr[n - 1];
+    cout<<ans<<endl;
+}
+                
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+   
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+auto start = std::chrono::system_clock::now() ;
+   
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+// -----for run-time calculation -------//
+auto end = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds = end-start;
+std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+#ifndef ONLINE_JUDGE
+cout<<"RUN TIME : "<<elapsed_seconds.count()<<"s"<<endl;
+#endif
+//---------//
+    return 0;
+}
